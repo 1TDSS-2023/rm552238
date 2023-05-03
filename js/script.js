986d82c2b74c5668e0851c9afa5201a1970aeb1d
Str1nder <rm552238@fiap.com.br>
2023-05-03 20:56:44 -0300

    defer/ addEventListener/getById/Math/Interpolação

--- js/script.js
+++ js/script.js
@@ -6,12 +6,40 @@
 
 //console.log(parseInt(nr1)+parseInt(nr2));
 
-let nome1 = "Dubas";
-// let nome2 = "Serbatinho";
-// const nome3 = "Julia";
+// let nome1 = "Dubas";
+// // let nome2 = "Serbatinho";
+// // const nome3 = "Julia";
 
-if(nome1 != ""){
-    let nome1 = "Julia";
-}
+// if(nome1 != ""){
+//     let nome1 = "Julia";
+// }
 
-console.log(nome1)
+// console.log(nome1)
+
+{/* <button value="click" id="meu-btn">Click</button> */}
+
+const btn = document.getElementById("meu-btn"); 
+
+//primeiro passo para a transformação do botão em rgb...
+
+btn.addEventListener("click", function(){
+    //console.log(this.textContent)
+    // Função matemática Math
+    // randon = Retorna um número aleatório entre 0 e 1. EX: 0,3151165184;
+    //floor = arredonda o numero para baixo
+    //ceil = Arredonda o numero para cima
+    //round = Arrendonda o numero aleatorio
+    
+    r = Math.round(Math.random() * 255);
+    g = Math.round(Math.random() * 255);
+    b = Math.round(Math.random() * 255);
+
+    // this.setAttribute("style","background-color:rgb("+r+","+g+","+b+");");
+    this.setAttribute("style",`background-color:rgb(${r},${g},${b});`);
+    
+
+});
+
+let r=0,g=0,b=0;
+
+
